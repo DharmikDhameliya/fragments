2ba2f09cc3dc96d0c95b96dd2b85b3f7d768be95
Dharmik Dhameliya <160946778+DharmikDhameliya@users.noreply.github.com>
2026-04-13 14:15:37 -0400

    final: assignment 3 ready

--- src/model/data/index.js
+++ src/model/data/index.js
@@ -5,7 +5,7 @@ const logger = require('../../logger');
 // If the environment sets an AWS Region, we'll use AWS storage
 // services (S3, DynamoDB); otherwise, we'll use an in-memory db.
 module.exports = (() => {
-  if (process.env.AWS_REGION) {
+  if (process.env.AWS_REGION && process.env.NODE_ENV !== 'test') {
     logger.info('Using AWS storage');
     return require('./aws');
   }
